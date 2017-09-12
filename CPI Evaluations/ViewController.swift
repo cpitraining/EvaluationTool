2235737e5bf570b6428528b5a3d38767b7ead045
Smith Frazier <fsmith@Smiths-MacBook-Pro.local>
2017-09-12 10:11:00 -0400

    Initial Commit

---
+++ CPI Evaluations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CPI Evaluations
+//
+//  Created by Smith, Frazier on 9/12/17.
+//  Copyright © 2017 CPI Security Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
